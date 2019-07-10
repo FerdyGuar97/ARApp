392f55652fbc8aa811657608bb7dd794a03c1cf7
Ferdinando Guarino <ferdinando.guarino97@icloud.com>
2019-07-10 15:01:25 +0200

    Initial commit

---
+++ ARApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARApp
+//
+//  Created by Ferdinando Guarino on 10/07/2019.
+//  Copyright © 2019 Ferdinando Guarino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
