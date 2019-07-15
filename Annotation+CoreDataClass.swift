dc5ee2d5a4b44c2a6059f6fc7452de58cc30e39f
Armando Falanga <armando.falanga@gmail.com>
2019-07-15 11:23:15 +0200

    Added annotation in Core Data

---
+++ Annotation+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Annotation+CoreDataClass.swift
+//  ARApp
+//
+//  Created by Armando Falanga on 15/07/2019.
+//  Copyright © 2019 Ferdinando Guarino. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Annotation)
+public class Annotation: NSManagedObject {
+
+}
---
+++ Annotation+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Annotation+CoreDataProperties.swift
+//  ARApp
+//
+//  Created by Armando Falanga on 15/07/2019.
+//  Copyright © 2019 Ferdinando Guarino. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Annotation {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Annotation> {
+        return NSFetchRequest<Annotation>(entityName: "Annotation")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var subtitle: String?
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+
+}
