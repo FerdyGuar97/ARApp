5e817485698c7bf6e608a57b8e3afff46815c735
Armando Falanga <armando.falanga@gmail.com>
2019-07-16 10:13:14 +0200

    Now you can add a pin everywhere on the map

--- ARApp/ViewController.swift
+++ ARApp/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Foundation
 
 class ViewController: UIViewController, MKMapViewDelegate {
 
@@ -19,15 +20,38 @@ class ViewController: UIViewController, MKMapViewDelegate {
     // Valore di default per l'ampiezza della regione da visualizzare
     let regionRadius: Double = 1200
     
-    // Richiesta all'utente di abilitare i permessi di geolocalizzazione
+    // Richiede all'utente di abilitare i permessi di geolocalizzazione
     func allowGPS() {
-        if (CLLocationManager.locationServicesEnabled()) {
-            locationManager.requestAlwaysAuthorization()
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined: // E' la prima volta che l'utente apre l'applicazione
             locationManager.requestWhenInUseAuthorization()
+        case .restricted,.denied: // L'utente ha disattivato la geolocalizzazione
+            self.openAlertToSettings(title: "Location in use disabled", description: "To enable location in usa change it in Settings")
+        case .authorizedWhenInUse, .authorizedAlways: // Tutto ok, la geolocalizzazione è attivata ed abbiamo i permessi
+            return;
+        default: print("Unknown GPS Auth status")
         }
     }
     
-    // Permette di centare la visuale sulla posizione dell'utente
+    // Apre un'alert che porta ai settings della geolocalizzazione
+    func openAlertToSettings(title: String, description: String) {
+        let alertController = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let openSettings = UIAlertAction(title: "Open Settings", style: .default) {
+            (action) in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertController.addAction(openSettings)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    // Permette di centrare la visuale sulla posizione dell'utente
     func centerPosition(regionRadius: Double) {
         /* Memorizza la posizione corrente */
         guard let coordinate = locationManager.location?.coordinate else {return}
@@ -47,10 +71,21 @@ class ViewController: UIViewController, MKMapViewDelegate {
         centerPosition(regionRadius: regionRadius)
     }
     
-    // Crea una nuova annotazione posizionandola sulla posizione corrente dell'utente
-    @IBAction func addAnnotation(_ sender: UIButton) {
+    @IBAction func longPressureAction(_ sender: UILongPressGestureRecognizer) {
+        let posMapView = sender.location(in: mapView)
+        let coordinate = mapView.convert(posMapView, toCoordinateFrom: mapView)
+        addAnnotation(at: coordinate)
+    }
+    
+    @IBAction func addBtnAction(_ sender: UIButton) {
+        let coordinate = mapView.userLocation.coordinate
+        addAnnotation(at: coordinate)
+    }
+    
+    // Crea una nuova annotazione posizionandola sulle coordinate passate come parametro
+    func addAnnotation(at coordinate: CLLocationCoordinate2D) {
         let newAnnotation = ARAppStdPointAnnotation()
-        newAnnotation.coordinate = mapView.userLocation.coordinate
+        newAnnotation.coordinate = coordinate
         
         let alert = UIAlertController(title: "Nuova annotazione", message: nil, preferredStyle: .alert)
         
@@ -76,11 +111,14 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
     }
     
+    // Viene richiamata ogni volta che un'annotazione deve essera visualizzata sulla mappa, tale annotazione verrà visualizzata come una Annotation View
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Serve per escludere l'annotazione riguardante la posizione attuale dell'utente
         if annotation is MKUserLocation {
             return nil;
         }
         
+        // Le Annotation View che sono fuori dalla regione visibile vengono marcate come riutilizzabili
         let reuseId = "reuse"
         if let anyAnn = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) {
             anyAnn.annotation = annotation;
@@ -90,10 +128,12 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let anyAnn = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
         anyAnn.canShowCallout = true;
         anyAnn.pinTintColor  = .green
+        // Questo parametro permette di fare lo spostamento dell'annotation view, la relativa annotazione verrà riscritta automaticamente
         anyAnn.isDraggable = true;
         return anyAnn;
     }
     
+    // Serve per gestire gli stati di un pin che viene spostato
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationView.DragState, fromOldState oldState: MKAnnotationView.DragState) {
         switch newState {
         case .starting:
@@ -106,6 +146,12 @@ class ViewController: UIViewController, MKMapViewDelegate {
         default: break
         }
     }
+
+    // Carica le annotazioni dal Core Data
+    func loadPointAnnotation() {
+        guard let points = CoreDataController.shared.getSavedAnnotations() else { return }
+        for i in points { mapView.addAnnotation(i) }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -114,11 +160,5 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.delegate = self
         loadPointAnnotation()
     }
-
-    func loadPointAnnotation() {
-        guard let points = CoreDataController.shared.getSavedAnnotations() else { return }
-        for i in points { mapView.addAnnotation(i) }
-    }
-    
 }
 
